eed0bbe415075704d84643973b1f2a1666cd349a
Denllar <denisgabbasov06.09.2004@gmail.com>
2024-01-01 18:37:32 +0500

    delete preload in html

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -62,7 +62,7 @@ const Cart = () => {
             <span> Сумма заказа: <b>{totalPrice} ₽</b> </span>
           </div>
           <div className="cart__bottom-buttons">
-            <Link to={"/"} className="button button--outline button--add go-back-btn">
+            <Link to={"/Jo-Jo-Pizza/"} className="button button--outline button--add go-back-btn">
               <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M7 13L1 6.93015L6.86175 1" stroke="#D3D3D3" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
               </svg>
