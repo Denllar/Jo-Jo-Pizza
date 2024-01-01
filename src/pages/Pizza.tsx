54c0164776998fdbcecba350493e22c669bd8142
Denllar <denisgabbasov06.09.2004@gmail.com>
2024-01-01 18:05:10 +0500

    fix image and link

--- src/pages/Pizza.tsx
+++ src/pages/Pizza.tsx
@@ -38,7 +38,7 @@ const Pizza: React.FC = () => {
                         <p><span>Состав:</span> {getPizza.compound}</p>
                     </div>
                     <div className="cart__bottom-buttons">
-                        <Link to={"/"} className="button button--outline button--add button--right go-back-btn">
+                        <Link to={"/Jo-Jo-Pizza/"} className="button button--outline button--add button--right go-back-btn">
                             <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M7 13L1 6.93015L6.86175 1" stroke="#D3D3D3" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
                             </svg>
